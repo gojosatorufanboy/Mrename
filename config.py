1363cd95bdb1d3c0c5f911c7e53619b102ff57bc
Gojo <101966680+gojosatorufanboy@users.noreply.github.com>
2024-07-24 09:36:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,8 +27,8 @@ class Config(object):
         admin) else admin for admin in os.environ.get('ADMIN', '7172796863').split()]  # ⚠️ Required
     
 
-    API = environ.get("API", "ccdd7d73458653f07aecbaa1d8159457965391b4") # shortlink api
-    URL = environ.get("URL", "publicearn.com") # shortlink domain without https://
+    API = os.environ.get("API", "ccdd7d73458653f07aecbaa1d8159457965391b4") # shortlink api
+    URL = os.environ.get("URL", "publicearn.com") # shortlink domain without https://
     VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
     BOT_USERNAME = environ.get("BOT_USERNAME", "SreeLeea_Renamebot") # bot username without @
     VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
